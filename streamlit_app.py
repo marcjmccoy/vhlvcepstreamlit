4b184da2750e3e210ffaaa43b3380f7f247356eb
marcjmccoy <marc.mccoy1@ucalgary.ca>
2025-11-25 13:32:17 -0500

    commit on main

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import re
 from vhl_pvs1 import classify_vhl_pvs1  # Import PVS1 classifier
 from vhl_ps1 import classify_vhl_ps1    # Import PS1 classifier
 
-st.title("VHL VCEP Variant Classifer")
+st.title("VHL VCEP Variant Classifer (v1.0 TEST) 🧬")
 
 st.markdown(
     """
@@ -13,22 +13,15 @@ st.markdown(
     Some suggested variants to try entering:
 
     - NM_000551.4(VHL):c.263+1G>A
-    - NM_000551.4(VHL):c.340-1G>A
     - NM_000551.4(VHL):c.119_340del
     - NM_000551.4(VHL):c.123+5G>A
-    - NM_000551.4(VHL):c.154-7A>G
-    - NM_000551.4(VHL):c.477del (p.Glu160fs)
-    - NM_000551.4(VHL):c.422dup (p.Asn141fs)
+    - NM_000551.4(VHL):c.1A>T (p.Met1Leu)
+    - NM_000551.4(VHL):c.160A>T (p.Met54Leu)
+    - NM_000551.4(VHL):c.150G>A (p.Pro50=)
     - NM_000551.4(VHL):c.408del (p.Phe136fs)
     - NM_000551.4(VHL):c.120_122del (p.Val41del)
     - NM_000551.4(VHL):c.190_195dup (p.Lys64_Leu65dup)
     - NM_000551.4(VHL):c.263G>A (p.Trp88Ter)
-    - NM_000551.4(VHL):c.208G>T (p.Glu70Ter)
-    - NM_000551.4(VHL):c.586A>T (p.Lys196Ter)
-    - NM_000551.4(VHL):c.583C>T (p.Gln195Ter)
-    - NM_000551.4(VHL):c.1A>T (p.Met1Leu)
-    - NM_000551.4(VHL):c.160A>T (p.Met54Leu)
-    - NM_000551.4(VHL):c.150G>A (p.Pro50=)
     """
 )
 
